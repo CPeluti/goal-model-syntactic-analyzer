b5bf70eaa553301c7a6860164c1e42ca4a965f35
Caio Peluti <cpeluti@gmail.com>
2022-09-08 19:16:36 -0300

    config do jest ajustada

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  modulePathIgnorePatterns: ["build", "core", "dist", "examples", "goal-model-examples", "test.ts"]
+  modulePathIgnorePatterns: ["build", "core", "dist", "examples", "goal-model-examples", "test.ts"],
+  moduleDirectories: ["node_modules", "core"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1"
+  }
 };
--- test/goal_model/goal_model.spec.ts
+++ test/goal_model/goal_model.spec.ts
@@ -1,9 +1,25 @@
 import {main} from "../../core/index"
+import {readFileSync} from 'fs'
 
 test('Room Cleaning', async () => {
     await main(
-        "../../goal-model-examples/Room Cleaning Example/hddl/Room Cleaning.hddl",
-        "../../goal-model-examples/Room Cleaning Example/gm/Room Cleaning.json",
-        "../../goal-model-examples/Room Cleaning Example/configs/Room Cleaning Config.json"
+        "/home/caio/goal-model-syntactic-analyzer/goal-model-examples/Room Cleaning Example/hddl/Room Cleaning.hddl",
+        "/home/caio/goal-model-syntactic-analyzer/goal-model-examples/Room Cleaning Example/gm/Room Cleaning.json",
+        "/home/caio/goal-model-syntactic-analyzer/goal-model-examples/Room Cleaning Example/configs/Room Cleaning Config.json"
     )
+    const result = JSON.parse(readFileSync('/home/caio/goal-model-syntactic-analyzer/goal-model-error-list.json', 'utf-8'))
+    
+    const test = [
+        {
+            "nodeId": "7f04d613-d3b0-4312-9ad8-43c7d2a0db3f",
+            "nodeName": "G2: Fetch Dirty Rooms",
+            "message": "1: world_db->seleact(r:Room | !r.is_clean)}\n^..........^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\nExpected : \"SELECT\" or \"PROPERTY\" got INVALID"
+        },
+        {
+            "nodeId": "a07c4186-f533-4a0a-afbe-6efcb81222f6",
+            "nodeName": "G3: Clean Rooms",
+            "message": "1: rooms->forAll}\n^.......^^^^^^^^\nExpected : \"PROPERTY\" got UNIVERSAL_CONDITION"
+        }
+    ]
+    expect(test).toEqual(result)
 });
